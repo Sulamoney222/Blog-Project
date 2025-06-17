ea68940f0585389814d222539c035bf58ddfd534
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-17 17:50:13 +0100

    new

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,22 @@
+'use client'
 import Image from "next/image";
 import TechImage from "../utils/tech-a.png"; // Correct way to import a local image
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Home() {
+  
+  useGSAP(()=>{
+  gsap.to('#read', {translateX:0, opacity:1})
+  gsap.to('#img', {translateY:0, opacity:1})
+  },[])
+   
   return (
     <section className="min-h-screen max-md:px-10 flex items-center justify-center bg-zinc-950 text-white px-4 py-16">
       <div className="max-w-6xl w-full grid md:grid-cols-2 gap-10 items-center">
         {/* Text Section */}
-        <div>
-          <h2 id='re' className="rem text-4xl sm:text-5xl font-bold mb-6 leading-tight">
+        <div id="read" className="opacity-0 translate-x-20">
+          <h2 id='re' className="text-4xl sm:text-5xl font-bold mb-6 leading-tight">
             Decode the Future.<br />One Line at a Time.
           </h2>
           <p className="text-lg sm:text-xl text-zinc-400 mb-8">
@@ -20,7 +29,7 @@ export default function Home() {
         </div>
 
         {/* Image Section */}
-        <div className="flex justify-center">
+        <div className="flex justify-center translate-y-16 opacity-0" id="read">
           <Image
             src={TechImage}
             alt="Tech Illustration"
