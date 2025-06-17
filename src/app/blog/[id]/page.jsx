f1be4ef1be90cbc98aa9a82547a19237f80b9f94
Sulaimon <sulaimonnasiru06@gmail.com>
2025-06-17 12:43:25 +0100

    new changes

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogPost = () => {
+  return (
+    <div>
+      <h3>bLOG POSTS</h3>
+    </div>
+  )
+}
+
+export default BlogPost
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    
+    <div className="">
+      <h2>Home Page</h2>
+    </div>
     </>
   );
 }
